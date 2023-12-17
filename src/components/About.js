1fb08d2bb94dee0869bea5caf645cf3d0c860fb5
Kaltuma777 <Kaltuma.abdi@student.moringaschool.com>
2023-12-17 11:25:00 +0300

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
 
-function About() {
+function About({bio}) {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>Put the bio in here</p>
+      {bio&&<p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
-}
+};
 
 export default About;
